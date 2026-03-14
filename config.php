845a373473f486ed8d43354fe0b7172ecd25567b
Bharatkumar <bharatkingdom@gmail.com>
2026-03-14 14:40:14 +0530

    fix asset base url

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 const DEMO_BASE_DIR = __DIR__;
-const DEMO_BASE_URL = '/demo';
+const DEMO_BASE_URL = '';
 
-const CLIENT_ID = 'YOUR_CLIENT_ID';
-const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const CLIENT_ID = '1b88a6d8-e904-4046-97f1-5537cfe9c827';
+const CLIENT_SECRET = 't4ADts9eegyqYtSr29WzITkyViwKqq02TN9FUMoT';
 
-const BACKGROUND_COLOR = ['#6e0fde', '#802cc7']; // single value provides solid background color to the element, use multiple values for gradient color
+const BACKGROUND_COLOR = ['#6e0fde', '#802cc7'];
 
 const BTN_BG_COLOR = '#f8cc3b';
 const ACTIVE_BTN_BG_COLOR = '#eab813';
