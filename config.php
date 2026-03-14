c5fc1c84ddca6ec123f7a4bc9f647e622cd9cb42
Bharatkumar <bharatkingdom@gmail.com>
2026-03-14 18:03:24 +0530

    Added ephemeris

--- config.php
+++ config.php
@@ -3,11 +3,9 @@
 const DEMO_BASE_DIR = __DIR__;
 const DEMO_BASE_URL = '';
 
-/* Swiss Ephemeris location */
-const SWE_PATH = DEMO_BASE_DIR . '/swisseph';
-const EPHE_PATH = DEMO_BASE_DIR . '/swisseph/ephe';
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
 
-/* Website UI colors */
 const BACKGROUND_COLOR = ['#6e0fde', '#802cc7'];
 
 const BTN_BG_COLOR = '#f8cc3b';
