40dbdb32f159146e24480e1c6022e74c091e5b92
Bharatkumar <bharatkingdom@gmail.com>
2026-03-14 17:57:01 +0530

    Added ephemeris

--- config.php
+++ config.php
@@ -3,10 +3,12 @@
 const DEMO_BASE_DIR = __DIR__;
 const DEMO_BASE_URL = '';
 
-const CLIENT_ID = '1b88a6d8-e904-4046-97f1-5537cfe9c827';
-const CLIENT_SECRET = 't4ADts9eegyqYtSr29WzITkyViwKqq02TN9FUMoT';
+/* Swiss Ephemeris location */
+const SWE_PATH = DEMO_BASE_DIR . '/swisseph';
+const EPHE_PATH = DEMO_BASE_DIR . '/swisseph/ephe';
 
-const BACKGROUND_COLOR = ['#6e0fde', '#802cc7']; // single value provides solid background color to the element, use multiple values for gradient color
+/* Website UI colors */
+const BACKGROUND_COLOR = ['#6e0fde', '#802cc7'];
 
 const BTN_BG_COLOR = '#f8cc3b';
 const ACTIVE_BTN_BG_COLOR = '#eab813';
